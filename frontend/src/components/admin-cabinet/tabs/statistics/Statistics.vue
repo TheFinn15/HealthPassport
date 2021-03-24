8942e00fcd338ebedfbed6d8cd87908863b12d1c
TheFinn15 <timlavr87@gmail.com>
2021-03-24 16:43:41 +0200

    frontend: added locale en, ru

--- frontend/src/components/admin-cabinet/tabs/statistics/Statistics.vue
+++ frontend/src/components/admin-cabinet/tabs/statistics/Statistics.vue
@@ -182,56 +182,6 @@ export default {
           name: this.locales.statistic[1].title,
           data: []
         }
-      ],
-      months: [
-        {
-          value: 0,
-          text: "Январь"
-        },
-        {
-          value: 1,
-          text: "Февраль"
-        },
-        {
-          value: 2,
-          text: "Март"
-        },
-        {
-          value: 3,
-          text: "Апрель"
-        },
-        {
-          value: 4,
-          text: "Май"
-        },
-        {
-          value: 5,
-          text: "Июнь"
-        },
-        {
-          value: 6,
-          text: "Июль"
-        },
-        {
-          value: 7,
-          text: "Август"
-        },
-        {
-          value: 8,
-          text: "Сентябрь"
-        },
-        {
-          value: 9,
-          text: "Октябрь"
-        },
-        {
-          value: 10,
-          text: "Ноябрь"
-        },
-        {
-          value: 11,
-          text: "Декабрь"
-        }
       ]
     };
   },
