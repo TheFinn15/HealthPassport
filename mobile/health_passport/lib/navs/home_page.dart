a7c4bcf2d73e3dbddfe434f4c316fd5e68b08171
TheFinn15 <timlavr87@gmail.com>
2021-03-29 02:15:37 +0300

    mobile: added basic home page

--- mobile/health_passport/lib/navs/home_page.dart
+++ mobile/health_passport/lib/navs/home_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<String> getToken() async {
@@ -17,25 +18,106 @@ class HomePage extends StatelessWidget {
         title: Text("Головна сторінка"),
         automaticallyImplyLeading: false
       ),
-      body:  FutureBuilder<String>(
-        future: getToken(),
-        builder: (context, AsyncSnapshot<String>snapshot) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            children = <Widget>[
-              Text(snapshot.data)
-            ];
-          } else {
-            children = <Widget>[
-              Text("Помилка")
-            ];
-          }
-          return Center(
-              child: children[0]
-          );
-        },
-      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "Мої дані",
+              style: GoogleFonts.roboto(fontSize: 30.0, fontWeight: FontWeight.bold),
+            )
+          ),
+         Container(
+           margin: EdgeInsets.fromLTRB(150, 10, 150, 10),
+           child:  Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             crossAxisAlignment: CrossAxisAlignment.start,
+             children: [
+               GestureDetector(
+                 child: Card(
+                   elevation: 8,
+                   shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadius.circular(5.0)
+                   ),
+                   child:  Row(
+                     mainAxisAlignment: MainAxisAlignment.start,
+                     children: [
+                       Padding(
+                         padding: const EdgeInsets.all(20),
+                         child: Text(
+                           "Хвороби",
+                           style: GoogleFonts.roboto(fontSize: 20.0, fontWeight: FontWeight.bold),
+                         ),
+                       ),
+                       Icon(Icons.keyboard_arrow_down, size: 30)
+                     ],
+                   ),
+                 ),
+                 onTap: () {
+                   print("TAPPED");
+                 },
+               ),
+               Card(
+                 elevation: 8,
+                 shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.circular(5.0)
+                 ),
+                 child: Row(
+                   mainAxisAlignment: MainAxisAlignment.start,
+                   children: [
+                     Padding(
+                       padding: const EdgeInsets.all(20),
+                       child: Text(
+                         "Обстеження",
+                         style: GoogleFonts.roboto(fontSize: 20.0, fontWeight: FontWeight.bold),
+                       ),
+                     )
+                   ],
+                 ),
+               ),
+               Card(
+                 elevation: 8,
+                 shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.circular(5.0)
+                 ),
+                 child: Row(
+                   mainAxisAlignment: MainAxisAlignment.start,
+                   children: [
+                     Padding(
+                       padding: const EdgeInsets.all(20),
+                       child: Text(
+                         "Вакцинації",
+                         style: GoogleFonts.roboto(fontSize: 20.0, fontWeight: FontWeight.bold),
+                       ),
+                     )
+                   ],
+                 ),
+               )
+             ],
+           ),
+         )
+        ],
+      )
       // bottomSheet: HomeBottomNav(),
     );
   }
 }
+
+// FutureBuilder<String>(
+// future: getToken(),
+// builder: (context, AsyncSnapshot<String>snapshot) {
+// List<Widget> children;
+// if (snapshot.hasData) {
+// children = <Widget>[
+// Text(snapshot.data)
+// ];
+// } else {
+// children = <Widget>[
+// Text("Помилка")
+// ];
+// }
+// return Center(
+// child: children[0]
+// );
+// },
+// ),
